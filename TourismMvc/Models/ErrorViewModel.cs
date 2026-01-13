021b66978ede478811bd980a4e4185c6e140360f
Aayush38-s <20027682@students.koi.edu.au>
2026-01-13 16:34:42 +1100

    Delete TourismMvc directory

--- TourismMvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TourismMvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
